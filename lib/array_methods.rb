5f16b49f592d203c9033ce1c1a976a0ed3bc048a
Plimon Howard V <plimon.howard@gmail.com>
2020-01-10 20:32:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,20 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times do |count|
+    if array[count] == value_to_find
+      return count
+    end
+  end
+    nil
 end
 
 def find_max_value(array)
+  hval = 0
+  array.length.times do |count|
+    if hval <= array[count]
+      hval = array[count]
+    end
+  end
+    return hval
   # Add your solution here
 end
 
